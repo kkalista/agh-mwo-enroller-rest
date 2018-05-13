48725c2e34e78c7d3940e2ce8d94762d4edddbd2
student1 <student1@s10n-mwo-16.pcoip.ki.agh.edu.pl>
2018-05-13 14:01:24 +0200

    POST /participants

--- src/main/java/com/company/enroller/controllers/ParticipantRestController.java
+++ src/main/java/com/company/enroller/controllers/ParticipantRestController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,5 +35,14 @@ public class ParticipantRestController {
 	     }
 	     return new ResponseEntity<Participant>(participant, HttpStatus.OK);
 	 }
+	
+	@RequestMapping(value = "", method = RequestMethod.POST)
+	 public ResponseEntity<?> addParticipant(@RequestBody Participant participant) {
+		if (participantService.findByLogin(participant.getLogin())!=null) {
+			return new ResponseEntity("Participant with login: "+ participant.getLogin()+ " already exist", HttpStatus.CONFLICT);
+		}
+		participantService.addParticipant(participant);
+		return new ResponseEntity<Participant>(participant, HttpStatus.OK);
+	}
 
 }
